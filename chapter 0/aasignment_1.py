6612cee8d6f769b3f6d6733ee1502849fb32504a
Sunkad <Apoorva.Sunkad@commscope.com>
2026-06-20 20:26:02 +0530

    Assignment to print something multiple times

---
+++ chapter 0/aasignment_1.py
@@ -0,0 +1,4 @@
+print("Apoorva learns Python!" + "\n" + "This is my first program in Python.")
+
+# Printing the below statement 10 times using string multiplication operator
+print(("I am excited to learn Python programming language." + "\n") * 10)
